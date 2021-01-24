824766ee5bce6af008c2e332b85a4ee78af6b02d
Reinhold <mail@rbeny.de>
2021-01-24 20:42:10 +0100

    Update app.js

--- website/app.js
+++ website/app.js
@@ -2,4 +2,65 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+const apiKey = '2ab26a436d58499cf7890d34e58ba3dd';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    const newZipCode = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    getWeather(baseURL, newZipCode, apiKey).then(function (data) {
+            postData('/add', {temp: data.main.temp, date: newDate, feelings: feelings});
+        }
+    ).then(updateUI())
+
+}
+
+// Async Get
+const getWeather = async (baseURL, zipCode, key) => {
+    const res = await fetch(baseURL + zipCode + ',de' + '&appid=' + key)
+    try {
+
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+// Async POST
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+        const allData = await request.json();
+        console.log(allData);
+        document.getElementById('temp').innerHTML = allData.temp;
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('content').innerHTML = allData.feelings;
+    }catch(error){
+        console.log("error", error);
+    }
+}
