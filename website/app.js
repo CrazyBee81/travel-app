43791ead22ddf6c8b7121ddd7296e6c9830090e8
Beny <beny@gujnm271116.global.intra.guj.com>
2021-02-01 15:05:47 +0100

    add package.json and exchange fetch for axios

--- website/app.js
+++ website/app.js
@@ -2,7 +2,7 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getDate() + '.' + d.getMonth() + 1  + '.' + d.getFullYear();
+let newDate = d.getDate() + '.' + d.getMonth() + 1 + '.' + d.getFullYear();
 
 const apiKey = '2ab26a436d58499cf7890d34e58ba3dd&units=metric';
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
@@ -15,18 +15,17 @@ function performAction(e) {
     getWeather(baseURL, newZipCode, apiKey).then(function (data) {
             postData('/add', {temp: data.main.temp, date: newDate, feelings: feelings});
         }
-    ).then(updateUI())
-
+    ).then(() => {
+        updateUI()
+    })
 }
 
 // Async Get
 const getWeather = async (baseURL, zipCode, key) => {
-    const res = await fetch(baseURL + zipCode + ',de' + '&appid=' + key)
+    const response = await axios.get(baseURL + zipCode + ',de' + '&appid=' + key)
     try {
-
-        const data = await res.json();
-        console.log(data)
-        return data;
+        console.log(response.data)
+        return response.data;
     } catch (error) {
         console.log("error", error);
         // appropriately handle the error
@@ -35,13 +34,12 @@ const getWeather = async (baseURL, zipCode, key) => {
 
 // Async POST
 const postData = async (url = '', data = {}) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    const response = await axios({
+        method: 'post',
+        url: url,
+        data: {
+            data
+        }
     });
 
     try {
@@ -55,9 +53,9 @@ const postData = async (url = '', data = {}) => {
 
 // updateUI
 const updateUI = async () => {
-    const response = await fetch('/all');
+    const response = await axios.get('/all');
     try {
-        const newData = await response.json();
+        const newData = await response.data;
         console.log(newData);
         document.getElementById('temp').innerHTML = Math.round(newData.temp) + '°C';
         document.getElementById('date').innerHTML = newData.date;
@@ -66,16 +64,3 @@ const updateUI = async () => {
         console.log("error", error);
     }
 }
-
-document.getElementById('generate').addEventListener('click', performAction);
-
-function performAction(e) {
-    const newZipCode = document.getElementById('zip').value;
-    const feelings = document.getElementById('feelings').value;
-    getWeather(baseURL, newZipCode, apiKey).then(function (data) {
-            postData('/add', {temp: data.main.temp, date: newDate, feelings: feelings});
-        }
-    ).then(updateUI())
-
-}
-
